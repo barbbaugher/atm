a0e81acd5de92ba518ae40fc4c11e81479605976
Barb Baugher <barbbaugher@gmail.com>
2017-09-06 18:06:44 -0400

    With exit loop

--- src/atm/ATMClientApp.java
+++ src/atm/ATMClientApp.java
@@ -14,32 +14,38 @@ public class ATMClientApp {
 
 		System.out.println("Allow access? " + telhio.allowAccess(enteredPin));
 
-		if (telhio.allowAccess(enteredPin) == true) {
+		while (telhio.allowAccess(enteredPin) == true) {
 			int menuEntry;
+
 			do {
-				System.out.println("What would you like to do next?");
+				System.out.println("What would you like to do?");
 				System.out.println("Press 1 to deposit funds");
 				System.out.println("Press 2 to withdraw funds");
 				System.out.println("Press 3 to check your balance");
 				System.out.println("Press 4 to exit");
 				menuEntry = input.nextInt();
 			} while (menuEntry != 1 && menuEntry != 2 && menuEntry != 3 && menuEntry != 4);
-
-			if (menuEntry == 1) {
+			
+			switch (menuEntry) {
+			case 1:
 				System.out.println("Please enter your deposit amount:");
 				double amount = input.nextDouble();
 				telhio.deposit(amount);
 				System.out.println("Your current account balance: " + telhio.balance);
-			} else if (menuEntry == 2) {
+				break;
+			case 2:
 				System.out.println("Please enter your withdrawal amount:");
-				double amount = input.nextDouble();
+				amount = input.nextDouble();
 				telhio.withdrawal(amount);
 				System.out.println("Your current account balance: " + telhio.balance);
-			} else if (menuEntry == 3) {
+				break;
+			case 3:
 				System.out.println("Your current account balance: " + telhio.balance);
-			} else if (menuEntry == 4) {
+				break;
+			case 4:
 				System.out.println("Thank you.  Have a nice day!");
 				System.exit(0);
+				break;
 			}
 
 		}
